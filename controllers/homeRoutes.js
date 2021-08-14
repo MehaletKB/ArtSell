3986be92c82b93d846850affb6ca98572964555d
Tyler Lawson <tylaw93@yahoo.com>
2021-08-14 09:34:28 -0500

    Added cookies

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,41 +1,55 @@
-const router = require('express').Router();
-const { Artwork, User } = require('../models');
+// eslint-disable-next-line new-cap
+const router = require("express").Router();
+const { Artwork, User } = require("../models");
 
-router.get('/', async(req, res) => {
+router.get("/", async (req, res) => {
+  res.render("homepage", {
+    // eslint-disable-next-line camelcase
+    logged_in: req.session.logged_in,
+  });
+});
 
+router.get("/artwork/:id", async (req, res) => {
+  // if user is logged in , track their views
 
-})
-
-
-router.get('/artwork/:id', async (req, res) => {
-  //if user is logged in , track their views
-  try{
+  try {
     const artworkData = await Artwork.findByPk(req.params.id, {
       inculde: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
-    const artwork = artworkData.get ({ plain: true});
-    res.render('artwork', {
-      ... artwork
+    const artwork = artworkData.get({ plain: true });
+    res.render("artwork", {
+      ...artwork,
     });
+  } catch (err) {
+    res.status(400).json(err);
   }
 });
 
-router.post('/artwork/:id', async (req, res) => {//add to cart
- // get user from req.session.user
- //get art from req.paramas.id
-  //update purchase to is_In_Cart
-})
+// router.post("/artwork/:id", async (req, res) => {
+// add to cart
+// get user from req.session.user
+// get art from req.paramas.id
+// update purchase to is_In_Cart
+// });
 
-router.get('/cart', (req, res) => {
-  //send back data for all purchases is_IN_Cart
-})
-router.post('/cart')//update purchases to bought
+// router.get("/cart", (req, res) => {
+// send back data for all purchases is_IN_Cart
+// });
+// router.post("/cart"); // update purchases to bought
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
+  res.render("login");
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: "Super secret secret",
@@ -23,6 +27,7 @@ app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
