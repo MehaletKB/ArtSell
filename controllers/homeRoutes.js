02073ac073b52f82c28ee97b5f06ebf703e42b6a
Dan Looten <danwlooten@gmail.com>
2021-08-17 19:18:47 -0500

    Update login to be continued

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -90,7 +90,7 @@ router.get("/cart", withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      include: [{ model: Post }],
+      include: [{ model: Artwork }],
     });
 
     const user = userData.get({ plain: true });
