e2e4464a002604dd99a029da3d1e74fc59fb2345
Dan Looten <danwlooten@gmail.com>
2021-08-18 03:56:57 -0500

    Added notes to complete addArt and deleteArt implementation on the front end

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,21 +1,30 @@
 // the purpose of this js is to receive post posts and comment deletions.
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+// TODO: associate the "id" to the user id
+// const id = req.session.user_id ???
+// TODO: associate the "data-id" with the artworkIds
+// http://localhost:3001/api/users/addArt/1
+//  "artworkIds": [6]
+//        --- or ---
+// const artId = event.target.getAttribute("data-id");
+//  "artworkIds": [artId]
 
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/users/deleteArt/${id}`, {
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
-      alert('Failed to delete post');
+      alert("Failed to delete post");
     }
   }
 };
 
 document
-  .querySelector('.artwork-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".artwork-list")
+  .addEventListener("click", delButtonHandler);
